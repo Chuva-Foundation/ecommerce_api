92758eab7c1558b663229321243ea2769f4b95d4
Jaylson Almeida <may2almeida@gmail.com>
2022-11-25 19:03:49 -0100

    updates
    -fixed auth

--- middleware/auth/index.js
+++ middleware/auth/index.js
@@ -3,17 +3,18 @@ const jwt = require('jsonwebtoken');
 module.exports = async (req, res, next) => {
 
     const authToken = req.headers.authorization;
+    if (!authToken) {
+        return res.status(401).json({error:"Token is required"});
+    }
 
     jwt.verify(authToken,process.env.AUTH_KEY, function (err, decoded) {
-        //console.log(err, decoded);
+        
         if (err) {
             return res.status(401).json({
                 error: 'Token Invalid'
             })
         }
-
         req.userId = decoded.id;
-        //console.log('UserID', decoded.id);
         next();
     });
 }
